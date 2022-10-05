3205782d4a11631090463b587d40962ce71561a7
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-10-05 16:41:24 -0300

    chore(app): graphql configuration + import Movies Module + app schema.gql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
+import { MoviesModule } from './movies/movies.module'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true
+    }),
+    PrismaModule,
+    MoviesModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
